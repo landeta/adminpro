1b9c7552cee1290ccd073ed3397c4a494c10e740
Oscar Ghisolfi <orghisolfi@gmail.com>
2018-01-16 00:51:29 -0300

    Agrrgamos y modificamos módulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  exports: [
+      BreadcrumbsComponent,
+      HeaderComponent,
+      SidebarComponent
+    ]
+})
+export class SharedModule {}
